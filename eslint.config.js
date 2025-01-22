00fa5ecf02792f1d352a39cff1634627f0ac738e
victorokonkwo67@gmail.com <victorokonkwo67@gmail.com>
2025-01-22 17:48:43 +0100

    fix: add rules

--- eslint.config.js
+++ eslint.config.js
@@ -14,5 +14,21 @@ export default [
 	{ files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
 	{
 		ignores: ['node_modules/', 'dist/', '.nuxt/', '*.config.js', '.output/']
+	},
+	{
+		rules: {
+			'vue/multi-word-component-names': 'warn',
+			'vue/no-v-html': 'warn',
+			'vue/component-tags-order': [
+				'error',
+				{
+					order: ['script', 'template', 'style']
+				}
+			],
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/no-explicit-any': 'off', // Allow the use of `any` type
+			'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // Ignore unused variables starting with "_"
+			'nuxt/no-cjs-in-config': 'off'
+		}
 	}
 ];
