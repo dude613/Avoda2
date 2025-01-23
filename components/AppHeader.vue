178ee9b1f9527995e98f6b6353b4cb913097a0bb
victorokonkwo67@gmail.com <victorokonkwo67@gmail.com>
2025-01-23 19:30:50 +0100

    chore: implement homepage setup for app

---
+++ components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+	<header class="bg-white shadow-sm">
+		<div class="flex p-5 items-center max-w-7xl mx-auto sm:p-5">
+			<p class="mr-auto font-bold capitalize">employee management system</p>
+			<nav class="flex">
+				<ul class="flex gap-3">
+					<li><nuxt-link to="/dashboard">dashboard</nuxt-link></li>
+					<li><nuxt-link to="/about">About</nuxt-link></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,11 @@ export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
+
 	vite: {
 		plugins: [tailwindcss()]
+	},
+	devServer: {
+		port: 4000
 	}
 });
