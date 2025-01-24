36e96ed2dd5fb6e3feb8fb6dc6433193bcbf13de
victorokonkwo67@gmail.com <victorokonkwo67@gmail.com>
2025-01-24 14:38:33 +0100

    chore: write composable(hook) to save items to localStorage
    add authentication component

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-type Variants = 'primary' | 'secondary';
+type Variants = 'primary' | 'secondary' | 'tertiary';
 
 // define props going into the button component
 defineProps({
@@ -17,12 +17,15 @@ defineProps({
 <template>
   <button
     :class="[
-      'px-5 py-2 capitalize flex space-x-5 text-sm rounded-sm focus-visible:outline-none disabled:pointer-events-none cursor-pointer justify-center items-center',
+      'px-5 py-3 capitalize flex space-x-5 text-sm rounded-sm focus-visible:outline-none disabled:pointer-events-none hover:cursor-pointer justify-center items-center',
       {
         'bg-white shadow-sm': variant === 'primary',
+        'bg-blue-500 shadow-sm text-white hover:bg-blue-700/95':
+          variant === 'tertiary',
         'text-white bg-black hover:bg-gray-900/90': variant === 'secondary',
       },
     ]"
+    v-bind="$attrs"
   >
     <slot name="icon" />
     <span>{{ text }}</span>
