6318516fec3e8af9ba89afa0ee1d0f12296ad698
victorokonkwo67@gmail.com <victorokonkwo67@gmail.com>
2025-02-07 13:52:34 +0100

    fix: cors improvement for development

--- avoda-be/src/main.ts
+++ avoda-be/src/main.ts
@@ -11,7 +11,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: '*',
+      origin: process.env.NODE_ENV !== Environment.PRODUCTION && '*',
       credentials: true,
     },
   });
