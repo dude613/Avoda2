7f269581ec3ee4519a0c5514b76091502633e78a
AG <info@caliber6.com>
2025-02-12 23:42:19 -0500

    chore: migrate to flat ESLint configuration with enhanced TypeScript and Prettier support

--- avoda-be/eslint.config.js
+++ avoda-be/eslint.config.js
@@ -1,25 +1,52 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
+const eslintPluginPrettier = require("eslint-plugin-prettier");
+const tsRecommended = require('@typescript-eslint/eslint-plugin').configs.recommended;
+const prettierRecommended = require('eslint-config-prettier');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+      globals: {
+        node: true,
+        jest: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      "prettier": eslintPluginPrettier,
+    },
+    rules: {
+      ...tsRecommended.rules,
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { vars: 'all', args: 'after-used', ignoreRestSiblings: true },
+      ],
+      "prettier/prettier": "error",
+      ...prettierRecommended.rules,
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    rules: {
+      'no-undef': 'warn',
+      'no-unused-vars': 'warn',
+    }
   },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+];
